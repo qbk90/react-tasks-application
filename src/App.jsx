f33705efeadad681ca6e54539f5ed7421a54c99e
qibeike <39752005+qibeike@users.noreply.github.com>
2024-02-08 16:57:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TaskList from "./TaskList";
+import TaskForm from './TaskForm'
+
+function App() {
+  return (
+    <div>
+      <TaskForm/>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primera tarea",
+    description: "Descripcion de mi primera tarea...",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Descripcion de mi segunda tarea...",
+  },
+  {
+    id: 2,
+    title: "Mi tercera tarea",
+    description: "Descripcion de mi tercera tarea...",
+  },
+  {
+    id: 3,
+    title: "Mi cuarta tarea",
+    description: "Descripcion de mi cuarta tarea...",
+  },
+];
